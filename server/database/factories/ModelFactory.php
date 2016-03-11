8d776b11ea54bf8dde0dcd5d6f9d0a8c26749cb4
Andrew Cleland <andrew.cleland@dsitia.qld.gov.au>
2016-03-11 14:54:26 +1000

    Add model factories, Improve Bubbler index tests

--- server/database/factories/ModelFactory.php
+++ server/database/factories/ModelFactory.php
@@ -19,3 +19,31 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Api\V1\Models\Bubbler::class, function (Faker\Generator $faker) {
+    return [
+        'description' => $faker->streetName,
+        'latitude' => $faker->latitude,
+        'longitude' => $faker->longitude,
+        'park_id' => factory(App\Api\V1\Models\Park::class)->create()->id
+
+    ];
+});
+
+$factory->define(App\Api\V1\Models\Park::class, function (Faker\Generator $faker) {
+    return [
+        'code' => $faker->numberBetween(0, 9999),
+        'name' => $faker->firstName,
+        'suburb_id' => factory(App\Api\V1\Models\Suburb::class)->create()->id
+    ];
+});
+
+$factory->define(App\Api\V1\Models\Suburb::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->lastName
+    ];
+});
+
+
+
+
--- server/tests/BubblerTest.php
+++ server/tests/BubblerTest.php
@@ -10,26 +10,14 @@ class BubblerTest extends TestCase
     use DatabaseMigrations;
 
     /**
-     * Ensure no more than 100 bubblers are returned when the latitude and longitude are not included in the request.
+     * GET /api/bubblers
      *
-     * @return void
-     */
-    public function testIndexWithoutLatLong()
-    {
-        $response = $this->get('/api/bubblers');
-        $response->assertResponseOk();
-
-        // Check the number of elements that are returned are less than 100
-        $jsonArray = json_decode($response->toString(), true);
-        $bubblersCount = count($jsonArray);
-        $this->assertLessThanOrEqual(100, $bubblersCount);
-    }
-
-    /**
      * Verify the structure of the returned json.
      */
     public function testIndexJsonStructure()
     {
+        factory(App\Api\V1\Models\Bubbler::class, 3)->create();
+
         $this->get('/api/bubblers')->seeJsonStructure([
             "data" => [
                 "*" => [
@@ -44,6 +32,44 @@ class BubblerTest extends TestCase
             ]
         ]);
     }
+
+    /**
+     * GET /api/bubblers
+     *
+     * Latitude and Longitude are not included in the request. It should return a list of 100 bubblers.
+     */
+    public function testIndexWithoutLatLong()
+    {
+        factory(App\Api\V1\Models\Bubbler::class, 150)->create();
+        $response = $this->call('GET', '/api/bubblers');
+
+        // Check the number of elements that are returned are less than 100
+        $jsonArray = json_decode($response->content(), true);
+        $bubblersCount = count($jsonArray["data"]);
+        $this->assertEquals(100, $bubblersCount);
+    }
+
+    /**
+     * GET /api/bubblers?latitude=0&longitude=0&radius=100
+     *
+     * Latitude, Longitude and Radius are included in the request. It should return bubblers that are near the coords.
+     */
+    public function testIndexWithLatLong()
+    {
+        // TODO: Find a way to shorten these declarations
+        factory(App\Api\V1\Models\Bubbler::class)->create(['latitude' => 0.00003, 'longitude' => 0.00003]);
+        factory(App\Api\V1\Models\Bubbler::class)->create(['latitude' => 0.00001, 'longitude' => 0.00001]);
+        factory(App\Api\V1\Models\Bubbler::class)->create(['latitude' => 0.00002, 'longitude' => 0.00002]);
+        //factory(App\Api\V1\Models\Bubbler::class)->create(['latitude' => 90, 'longitude' => 0]);
+
+        $response = $this->call('GET', '/api/bubblers?latitude=0&longitude=0&radius=100');
+
+        $jsonArray = json_decode($response->content(), true);
+        $bubblersCount = count($jsonArray["data"]);
+        $this->assertEquals(3, $bubblersCount);
+    }
+
+
 
 
 }
